0dd09317d5cf39d7cbdb9a03c8d645a7c7a0b905
Darion Welch <darion@getplatypi.com>
2015-04-10 18:21:06 -0500

    added login and profile viewcontrols

---
+++ public/viewcontrols/login/login.viewcontrol.ts
@@ -0,0 +1,15 @@
+﻿/// <reference path="../../_references.d.ts" />
+
+import plat = require('platypus');
+
+import BaseViewControl = require('../../viewcontrols/base/base.viewcontrol');
+
+class LoginViewControl extends BaseViewControl {
+    templateString: string = require('./login.viewcontrol.html');
+    context: any = { };
+    
+}
+
+plat.register.viewControl('login-vc', LoginViewControl);
+
+export = LoginViewControl;

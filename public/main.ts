6bc335bd84476245cded1d04a1c5e831366bf522
Will Johnston <will@getplatypi.com>
2015-04-10 15:03:21 -0500

    initial project

---
+++ public/main.ts
@@ -0,0 +1,12 @@
+/**
+ * This is the entry-point for Browserify. This file can maintain references to all your 
+ * custom controls, injectables, and other files that won't necessarily be loaded from files
+ * included in this file.
+ */
+
+// libraries
+require('platypus');
+require('platypusui');
+
+// app
+require('./app/app');
---
+++ public/services/base/base.service.ts
@@ -0,0 +1,19 @@
+﻿/// <reference path="../../_references.d.ts" />
+
+import plat = require('platypus');
+
+class BaseService {
+    protected static _inject: any = {
+        _http: plat.async.Http,
+        _Promise: plat.async.IPromise,
+        _utils: plat.Utils
+    };
+
+    protected _http: plat.async.Http;
+    protected _Promise: plat.async.IPromise;
+    protected _utils: plat.Utils;
+
+    host: string = 'my-host';
+}
+
+export = BaseService;
